2007d3dc4e42c20fe141a39a11aebf02453a9954
Janez Cergolj <me@jcergolj.me.uk>
2021-10-27 11:27:33 +0200

    add user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if (app()->isProduction()) {
+            return;
+        }
+
+        User::factory()
+            ->create([
+                'email' => 'me@jcergolj.me.uk',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' =>  null,
+            ]);
+    }
+}
